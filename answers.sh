2ed575e5b77a4fd58c9a922dd88104d94658093a
tareqelali <elalitareq@gmail.com>
2022-12-10 20:39:40 +0200

    everything is done

--- answers.sh
+++ answers.sh
@@ -1,11 +1,11 @@
 /*Basic Querries*/
-SELECT * from students;
-SELECT * from students where age>30;
-SELECT * from students where gender="F";
-SELECT Points from students where name="Alex";
-insert into students (name,age,gender,points) values("Tareq","26","M","200");
-UPDATE students set Points=Points+5 where name="Basma";
-UPDATE students set Points=Points-5 where name="Alex";
+/*1.*/ SELECT * from students;
+/*2.*/ SELECT * from students where age>30;
+/*3.*/ SELECT * from students where gender="F";
+/*4.*/ SELECT Points from students where name="Alex";
+/*5.*/ insert into students (name,age,gender,points) values("Tareq","26","M","200");
+/*6.*/ UPDATE students set Points=Points+5 where name="Basma";
+/*7.*/ UPDATE students set Points=Points-5 where name="Alex";
 /*Creating Table*/
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
@@ -15,22 +15,22 @@ CREATE TABLE graduates(
         Points INTEGER,
         Graduation TEXT
 );
-insert into graduates(name,age,gender,points) SELECT name,age,gender,points from students where name = "Layal";
-UPDATE graduates set Graduation = "08/09/2018" where name="Layal";
-Delete from students where name="Layal";
+/*10.*/ insert into graduates(name,age,gender,points) SELECT name,age,gender,points from students where name = "Layal";
+/*11.*/ UPDATE graduates set Graduation = "08/09/2018" where name="Layal";
+/*12.*/ Delete from students where name="Layal";
 /*Joins*/
-CREATE TABLE "empcomp" (
+/*14.*/CREATE TABLE "empcomp" (
 	"Employee Name" TEXT NOT NULL UNIQUE,
 	"Company Name" TEXT NOT NULL,
 	"Company Date" INTEGER
-);
-insert into empcomp ("Employee Name","Company Name") SELECT name,Company from employees;
-UPDATE empcomp SET "Company Date"="Date" from companies where "Company Name"="Name";
-SELECT "Employee Name" FROM empcomp where "Company Date"<"2000";
-SELECT "Company Name" FROM empcomp inner join employees on employees."Name"=empcomp."Employee Name" where Role="Graphic Designer";
+    );
+    insert into empcomp ("Employee Name","Company Name") SELECT name,Company from employees;
+    UPDATE empcomp SET "Company Date"="Date" from companies where "Company Name"="Name";
+/*15.*/SELECT "Employee Name" FROM empcomp where "Company Date"<"2000";
+/*16.*/SELECT "Company Name" FROM empcomp inner join employees on employees."Name"=empcomp."Employee Name" where Role="Graphic Designer";
 /*Count & Filter*/
-SELECT Name from students where Points=(SELECT max(Points) FROM students);
-SELECT avg(Points) FROM students;
-SELECT count(Points) from students WHERE Points="500";
-select count("Name") from students where instr(name,"s");
-select * from students order by Points desc;
+/*18.*/SELECT Name from students where Points=(SELECT max(Points) FROM students);
+/*19.*/SELECT avg(Points) FROM students;
+/*20.*/SELECT count(Points) from students WHERE Points="500";
+/*21.*/select count("Name") from students where instr(name,"s");
+/*22.*/select * from students order by Points desc;
